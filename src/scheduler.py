d9745e6d934d8ad2ffabeae5f04a702d6eae51a0
andersonramirez1 <ar2527@cornell.edu>
2025-05-02 22:14:14 -0400

    updated scheduler.py

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,12 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from helper import (main, calculate_general_room_availability, calculate_library_general_availability)
+from helper import (main, calculate_general_room_availability, calculate_library_general_availability, determine_hours)
 import time
 
 def update_everything():
-    print("Running library data update...")
     main()
     calculate_general_room_availability()
     calculate_library_general_availability(7, 10)
+    determine_hours()
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(update_everything, 'interval', minutes=5)
