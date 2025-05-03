47328a4e2e29832b74045f815b2a359ea43cb3c0
andersonramirez1 <ar2527@cornell.edu>
2025-05-02 22:15:40 -0400

    updated scheduler.py spec

--- src/scheduler.py
+++ src/scheduler.py
@@ -3,13 +3,16 @@ from helper import (main, calculate_general_room_availability, calculate_library
 import time
 
 def update_everything():
+    """
+    Runs helper.py main functions, in an interval of 10 minutes.
+    """
     main()
     calculate_general_room_availability()
     calculate_library_general_availability(7, 10)
     determine_hours()
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(update_everything, 'interval', minutes=5)
+scheduler.add_job(update_everything, 'interval', minutes=10)
 scheduler.start()
 
 while True:
